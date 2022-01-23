0925dd78fb48230c612916c0d575e2dd7ad68e4a
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-01-23 14:04:41 +0530

    Added solution - LeetHub

---
+++ Rearrange a string - GFG/rearrange-a-string.java
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Solution obj = new Solution();
+                    System.out.println(obj.arrangeString(s));
+                }
+                
+        }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public String arrangeString(String s)
+        {
+            //code here.
+            int sum = 0  ; 
+            for(int i=0;i<s.length();i++){
+                
+                if(s.charAt(i)>='0' && s.charAt(i)<='9'){
+                    sum+=s.charAt(i)-'0'  ; 
+                }
+            }
+            
+            char ch[] = s.toCharArray()  ;  
+            Arrays.sort(ch)  ; 
+            String res=""  ; 
+            for(int i=0;i<ch.length;i++){
+                if(ch[i]>='A' && ch[i]<='Z'){
+                    res+=ch[i]  ; 
+                }
+            }
+            
+            return res+sum ; 
+        }
+}
