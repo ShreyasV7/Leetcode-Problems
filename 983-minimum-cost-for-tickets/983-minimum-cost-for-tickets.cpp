00463e6c63af49ece9c9aa2627e17482742d4b94
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-02-15 11:03:49 +0530

    Time: 10 ms (25.43%), Space: 9.5 MB (71.41%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.cpp
@@ -1,27 +1,26 @@
 class Solution {
-private:
-    int minimumCost(vector<int>&days, vector<int>&costs, int ind,int nextDay,vector<int>&data){
-        if(ind >= days.size())
-            return 0 ; 
-        
-         if(days[ind] < nextDay)
-            return  minimumCost(days,costs,ind+1,nextDay,data)  ;
-        
-        if(data[ind]!=-1) return data[ind]  ; 
-        
-        int mn1 = costs[0]+minimumCost(days,costs,ind+1,days[ind]+1,data)  ; 
-        int mn2 = costs[1]+minimumCost(days,costs,ind+1,days[ind]+7,data)  ; 
-        int mn3 = costs[2]+minimumCost(days,costs,ind+1,days[ind]+30,data)  ; 
-        
-        return data[ind] = min(mn1,min(mn2,mn3))  ; 
-    }
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         
-        int n = days.size()  ;
-        vector<int>data(366,-1)  ; 
+        int maxDay = days.back()  ; 
+        vector<int>dp(365+1, 0)  ;  
         
-        int ans = minimumCost(days,costs,0,0,data)  ; 
-        return ans; 
+        int j = 0  ; 
+        for(int i=1 ; i <= maxDay; i++){
+            
+            if(i==days[j]){
+                j++ ; 
+                int mn1 = costs[0] + dp[i-1]   ; 
+                int mn2 = costs[1] +dp[i-min(i,7)]   ; 
+                int mn3 = costs[2] + dp[i-min(i,30)]  ; 
+                
+                dp[i] = min({mn1,mn2,mn3})  ;  
+            }
+            else{
+                dp[i] = dp[i-1]  ; 
+            }
+        }
+        
+        return dp[maxDay] ; 
     }
 };
