d6a34c45c8d5ae36f63b65cd88dc25b31031b5c5
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-02-19 12:34:56 +0530

    Time: 574 ms (25.16%), Space: 121.3 MB (19.87%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
     int minimumDeviation(vector<int>& nums) {
-       
+        
         int n = nums.size()  ; 
+        set<int>st; 
         
-        int mn = INT_MAX; 
         for(int i=0;i<n;i++){
-            if(nums[i] % 2 == 1) nums[i]*=2 ; 
+            if(nums[i] % 2) nums[i] = nums[i] * 2 ; 
+            st.insert(nums[i])  ; 
         }
         
-       priority_queue<int>pq;  
-        for(int i=0;i<n;i++){
-            mn = min(mn,nums[i])  ; 
-            pq.push(nums[i])  ;  
+        int currDiff , result = INT_MAX ; 
+        while(true){
+            int maxNum = *st.rbegin()  ; 
+            int minNum = *st.begin()  ;
+            currDiff = maxNum-minNum ; 
+            result = min(result, currDiff)  ; 
+            
+            if(maxNum % 2 == 0){
+                auto it = st.rbegin()  ; 
+                st.erase(*it)  ; 
+                st.insert(maxNum/2)  ; 
+            }
+            else{
+                break ; 
+            }
         }
         
-//         for(int i=0;i<n;i++){
-//             if(nums[i] != mn){
-//                 pq.push(nums[i]) ; 
-//             }
-//         }
-        
-        
-        int res = INT_MAX;  
-        while(pq.top() % 2 == 0){
-            res = min(res, pq.top()-mn)  ;
-            mn = min(mn,pq.top()/2)  ; 
-            pq.push(pq.top()/2) ; 
-            pq.pop()  ; 
-        }
-        
-        return min(res,pq.top()-mn) ; 
+        return result; 
     }
 };
