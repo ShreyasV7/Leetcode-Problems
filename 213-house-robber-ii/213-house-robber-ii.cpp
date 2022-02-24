e1b156b0badfd722b1ae9dcb6c8865f66c88f3c5
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-02-24 15:37:23 +0530

    Time: 4 ms (39.66%), Space: 7.7 MB (83.41%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,47 +1,51 @@
-// Tabulation Method 
+// Space Optimization
 class Solution {
 private:
-    int excludingLastIndex(vector<int>&nums){
-        int n = nums.size()  ; 
-        vector<int>dp1(n)  ; 
+    int excludingFirstIndex(vector<int>&nums){
         
-        dp1[0] = nums[0]  ; 
-        dp1[1] = max(nums[0],nums[1])  ; 
+        int curr , prev , beforeprev ; 
+        
+        int n = nums.size()  ; 
+        
+        beforeprev = nums[0]  ; 
+        prev = max(nums[0],nums[1])  ; 
         
         for(int i=2;i<n-1;i++){
-            dp1[i] = max(dp1[i-2]+nums[i] , dp1[i-1])  ; 
+            curr = max(prev , beforeprev+nums[i]) ; 
+            beforeprev = prev ; 
+            prev = curr ; 
         }
         
-        return dp1[n-2]  ; 
+        return prev ; 
     }
     
-    int excludingFirstIndex(vector<int>&nums){
-        int n = nums.size()  ; 
-        vector<int>dp2(n) ; 
+    int excludingLastIndex(vector<int>&nums){
         
-        dp2[1] = nums[1]  ; 
-        dp2[2] = max(nums[1],nums[2])  ; 
+        int curr , prev , beforeprev ; 
+        
+        int n = nums.size() ; 
+        
+        beforeprev = nums[1]  ; 
+        prev = max(nums[1],nums[2])  ; 
         
         for(int i=3;i<n;i++){
-            dp2[i] = max(dp2[i-2]+nums[i] , dp2[i-1])  ;
+            curr = max(prev , beforeprev+nums[i]) ; 
+            beforeprev = prev ; 
+            prev = curr ; 
         }
         
-        return dp2[n-1]  ; 
+        return prev ; 
     }
-    
 public:
     int rob(vector<int>& nums) {
         
         int n = nums.size()  ; 
-        if(n==1) return nums[0]  ;
-        if(n==2) return max(nums[1],nums[0])  ; 
+        if(n==1) return nums[0]  ; 
+        if(n==2) return max(nums[0],nums[1])  ; 
         
-        int res1 = excludingLastIndex(nums)  ; 
-        int res2 = excludingFirstIndex(nums)  ; 
+        int res1 = excludingFirstIndex(nums)  ; 
+        int res2 = excludingLastIndex(nums)  ; 
         
         return max(res1,res2)   ;
-        
-        // Time complexity = O(n)
-        // Space complexity = O(n)
     }
 };
