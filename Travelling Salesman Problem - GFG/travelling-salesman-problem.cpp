5b608dd5ae3e2526baa4955522f6617e44c96854
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-03-02 12:57:43 +0530

    Added solution - LeetHub

---
+++ Travelling Salesman Problem - GFG/travelling-salesman-problem.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution {
+public:
+int dp[11][1<<11];
+
+int f(int i, int mask, vector<vector<int>> &cost) {
+    if(mask  == 0) return cost[i][0];
+    if(dp[i][mask] != -1) return dp[i][mask];
+    
+    int curAns = INT_MAX;
+    for(int j = 31; j >= 0; j--) {
+        if((mask & (1 << j))) {
+            curAns = min(curAns, cost[i][j] + f(j, mask ^ (1 << j), cost));
+        }
+    }
+    return dp[i][mask] = curAns;
+    
+}
+
+int total_cost(vector<vector<int>>cost){
+    memset(dp, -1, sizeof(dp));
+    int n = cost.size();
+    int current_mask = (1 << n) - 1;
+    current_mask = current_mask ^ (1 << 0);
+    return f(0, current_mask, cost);
+    
+}
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>cost(n, vector<int>(n, 0));
+		for(int i = 0; i < n; i++)
+			for(int j = 0; j < n; j++)cin >> cost[i][j];
+		Solution obj;
+		int ans = obj.total_cost(cost);
+		cout << ans <<"\n";
+	}
+	return 0;
+}  // } Driver Code Ends
