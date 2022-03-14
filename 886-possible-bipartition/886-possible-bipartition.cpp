b20bb4217fb9d2c62eea0a60877b3e98eaac8ad0
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-03-15 01:09:10 +0530

    Time: 209 ms (80.59%), Space: 63.9 MB (87.20%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.cpp
+++ 886-possible-bipartition/886-possible-bipartition.cpp
@@ -1,49 +1,49 @@
-class Solution {
-private:
-    bool bfs(int &src, vector<int>adj[] , vector<int>&color){
+class Graph{
+    public:
+    void createAdjacencyList(vector<vector<int>>&dislikes, vector<int>adj[]){
+        for(int i=0;i<dislikes.size(); ++i){
+            adj[dislikes[i][0]].push_back(dislikes[i][1]) ; 
+            adj[dislikes[i][1]].push_back(dislikes[i][0]) ;
+        }
+    }
+    
+    bool searchFunction(vector<int>adj[],int sourceNode, vector<int>&color){
         
-        queue<int>data;  
+        if(color[sourceNode] == -1)
+        color[sourceNode] = 1 ; 
         
-        data.push(src) ;
-        
-        color[src] = 1 ; 
-        
-        while(!data.empty()){
-            int currNode = data.front()  ; 
-            data.pop()  ; 
-            
-            for(auto child : adj[currNode]){
-                if(color[child] == -1){
-                    color[child] = 1 - color[currNode]  ; 
-                    data.push(child) ; 
-                }
-                else if(color[child] == color[currNode]){
+        for(auto child : adj[sourceNode]){
+            if(color[child] == -1){
+                color[child] = 1-color[sourceNode]  ; 
+                if(searchFunction(adj,child,color) == 0) 
                     return false; 
-                }
+            }
+            else if(color[child]==color[sourceNode]){
+                return false ;
             }
         }
         
-        return true; 
+        return true ;
     }
+    
+} ;  
+class Solution {
 public:
-    bool possibleBipartition(int n, vector<vector<int>>& d) {
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         
-        vector<int>adj[n+2]  ; 
+        Graph obj ; 
+        vector<int>adj[n+1]  ; 
         
-        for(int i=0;i<d.size();i++){
-            adj[d[i][0]].push_back(d[i][1]) ;  
-            adj[d[i][1]].push_back(d[i][0]) ;
-        }
+        obj.createAdjacencyList(dislikes,adj)  ; 
         
         vector<int>color(n+1,-1)  ; 
         
         for(int i=1;i<=n;i++){
             if(color[i]==-1){
-                if(!bfs(i,adj,color)){
+                if(!obj.searchFunction(adj,i,color))
                     return false; 
-                }
             }
         }
-        return true;  
+        return true; 
     }
 };
