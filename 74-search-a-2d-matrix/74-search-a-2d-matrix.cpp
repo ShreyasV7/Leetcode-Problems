51fee36bfc421309b0e5fef81fdf149284d4bfa9
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-03-19 01:43:41 +0530

    Time: 4 ms (78.25%), Space: 9.6 MB (5.57%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -5,12 +5,18 @@ public:
         int row = matrix.size()  ; 
         int col = matrix[0].size()  ;  
         
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(matrix[i][j]==target){
-                    return 1 ; 
-                }
+        for(int i=0;i<row;++i){
+            
+            int l = 0, h = matrix[i].size()-1 ; 
+            int ans ; 
+            
+            while(l<=h){
+                int mid = (l+h)/2 ; 
+                if(matrix[i][mid] == target) return 1;  
+                if(matrix[i][mid] > target) h = mid-1 ;
+                else l = mid+1; 
             }
+            
         }
         
         return 0 ; 
