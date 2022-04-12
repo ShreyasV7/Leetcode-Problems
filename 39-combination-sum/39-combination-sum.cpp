efd98b1d8a4d7d9a9a05ed6b2a3c3cebcbc68667
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-04-13 02:04:31 +0530

    Time: 11 ms (59.19%), Space: 10.8 MB (71.69%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    void helper(vector<int>&nums, int indx, int target, int sum, vector<int>&temp, vector<vector<int>>&arr){
+        if(sum==target){
+            arr.push_back(temp)   ;  
+            return  ; 
+        }
+        
+        if(indx >= nums.size() || sum>target){
+            return ; 
+        }
+        temp.push_back(nums[indx])  ; 
+        helper(nums,indx,target,sum+nums[indx], temp,arr) ; 
+        temp.pop_back() ; 
+        
+        
+        helper(nums,indx+1,target,sum,temp,arr)   ; 
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>&nums, int target) {
+        
+        sort(nums.begin(),nums.end())  ; 
+        
+        vector<vector<int>>arr ;  
+        vector<int>temp ; 
+        helper(nums,0,target,0,temp,arr)  ;  
+        
+        return arr ;
+    }
+};
