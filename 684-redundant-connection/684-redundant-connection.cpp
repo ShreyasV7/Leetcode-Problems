e7ffb344580553a9a68b7f88a6812ab942e0b6be
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-05-03 12:12:24 +0530

    Time: 13 ms (38.10%), Space: 9.7 MB (22.51%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,32 +1,45 @@
 class Solution {
 private:
-    bool dfs(int node, int par, vector<vector<int>>&adj, vector<int>&vis){
-        
-        vis[node] = 1; 
-        for(auto &child : adj[node]){
-            if(vis[child]==0){
-                if(dfs(child,node,adj,vis)) return 1;  
-            }
-            else if(child != par) return 1;  
+    int start = -1 ; 
+    void dfs(int node, int par, vector<vector<int>>&adj, vector<int>&vis, set<int>&cycleNodes){
+        if(vis[node]==1){
+            start = node; 
+            return ; 
         }
-        return 0 ; 
+        
+        vis[node] = 1;  
+        
+        for(auto &c : adj[node]){
+            if(c==par) continue;  
+            if(cycleNodes.empty()) dfs(c,node,adj,vis,cycleNodes)  ; 
+            if(start!=-1) cycleNodes.insert(node)  ;
+            if(node==start){
+                start = -1; 
+                return; 
+            }
+        }
     }
 public:
+    
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         
         int n = edges.size()  ; 
-        vector<vector<int>>adj(n+1)  ;  
+        
+        vector<vector<int>>adj(n+1)  ; 
+        vector<int>vis(n+1,0) ; 
         
         for(auto &it : edges){
-           vector<int>vis(n+1,0)  ; 
-            int source = it[0]  ; 
-            int dest = it[1]  ; 
-            adj[source].push_back(dest)  ; 
-            adj[dest].push_back(source)  ;   
-           
-           if(dfs(source,-1,adj,vis)) return it;   
+            adj[it[0]].push_back(it[1])  ; 
+            adj[it[1]].push_back(it[0])  ; 
         }
         
+        set<int>cycleNodes; 
+        dfs(1,-1,adj,vis,cycleNodes);
+        
+        for(int i=n-1;i>=0;i--){
+            if(cycleNodes.count(edges[i][0]) && cycleNodes.count(edges[i][1])) 
+                return {edges[i][0] , edges[i][1]} ;  
+        }
         return {}  ; 
     }
 };
