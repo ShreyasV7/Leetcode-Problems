21737c3eee0081fcd517dd14d4a927052b735ea4
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-06-19 02:34:49 +0530

    Time: 1358 ms (5.27%), Space: 150.2 MB (63.53%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -1,37 +1,29 @@
 class WordFilter {
 public:
-    unordered_map<string, int> mp; //stores preffix and suffix sequence in it
-    
-    WordFilter(vector<string>& words) 
-    {
-        int n = words.size();
+    unordered_map<string,int>mp ;  
+    WordFilter(vector<string>& words) {
         
-        for(int i=0; i<n; i++) //cover each word in words
-        {
-            string word = words[i]; 
-            int wordsize = word.size();
-            
-            //storing all possible sequence of prefix and suffix of a word in the hashmap
-            for(int j=1; j<=wordsize; j++) //for preffix 
-            {
-                string pre = word.substr(0,j);
-                
-                for(int k=0; k<wordsize; k++) //for suffix
-                {
-                    string suff = word.substr(k, wordsize);
-                    mp[pre + "|" + suff] = i+1; //set index i+1 for all possible sequence of a word
-                    //we set i+1 to handle i=0 because by-default map initialized with 0 
+        for(int i=0;i<words.size();++i){
+            int m = words[i].size()  ; 
+            string curr=words[i]  ; 
+            for(int j=1;j<=m;j++){
+                string f1=curr.substr(0,j)  ;
+                for(int k =0 ;k<m;k++){
+                     string f2 = curr.substr(k,m)  ; 
+                      mp[f1+"-"+f2] = i+1 ; 
+                      //cout << f1 <<" "<< f2 <<"\n"  ; 
                 }
             }
         }
     }
     
-    int f(string prefix, string suffix) 
-    {
-        string s = prefix + "|" + suffix;
-        return mp[s]-1; //if sequence found in hashmap we return its index
+    int f(string prefix, string suffix) {
+        
+        //if(mp[prefix+"-"+suffix]==0) return -1 ; 
+        return mp[prefix+"-"+suffix]-1 ; 
     }
 };
+
 /**
  * Your WordFilter object will be instantiated and called as such:
  * WordFilter* obj = new WordFilter(words);
