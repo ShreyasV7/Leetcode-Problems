39836dd4bfee1bd2883d7bc0c7dd7b250f7af783
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-07-28 17:42:48 +0530

    Time: 88 ms (12.42%), Space: 13.4 MB (24.68%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,36 +1,36 @@
 class Solution {
-private:
-    void dfs(int i,int j,vector<vector<char>>& grid,vector<vector<bool>>&visit,int n, int m){
-        
-        if(i<0 || i>=n || j<0 || j>=m || visit[i][j] == 1) return ; 
-        
-        visit[i][j] = true ;
-
-        if(i+1 < n && grid[i+1][j]=='1') dfs(i+1,j,grid,visit,n,m)  ; 
-        if(i-1 >= 0 && grid[i-1][j]=='1') dfs(i-1,j,grid,visit,n,m)  ; 
-        if(j+1 < m && grid[i][j+1]=='1') dfs(i,j+1,grid,visit,n,m)  ; 
-        if(j-1 >= 0 && grid[i][j-1]=='1') dfs(i,j-1,grid,visit,n,m)  ; 
-    }
 public:
+    void dfs(int i,int j,vector<vector<int>>&vis,vector<vector<char>>&grid){
+        
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size()) return ; 
+        
+        if(grid[i][j]=='0' || vis[i][j]==1) return  ; 
+        
+        vis[i][j] = 1; 
+        
+        dfs(i+1,j,vis,grid)  ;
+        dfs(i-1,j,vis,grid)  ;
+        dfs(i,j+1,vis,grid)  ;
+        dfs(i,j-1,vis,grid)  ;
+        
+    }
     int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size() , m = grid[0].size()  ; 
         
-        int n = grid.size()  ; 
-        int m = grid[0].size()  ; 
+        vector<vector<int>>vis(n,vector<int>(m,0))  ; 
         
-        vector<vector<bool>>visit(n,vector<bool>(m,false))  ;
-        
-        int cnt = 0;
+        int cnt=  0 ; 
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' && visit[i][j]==false){
-                    
-                    dfs(i,j,grid,visit,n,m) ; 
+                
+                if(grid[i][j]=='1' && vis[i][j]==0){
+                    dfs(i,j,vis,grid)  ;
                     cnt++ ; 
                 }
             }
         }
         
-        return cnt;  
+        return cnt; 
     }
 };
