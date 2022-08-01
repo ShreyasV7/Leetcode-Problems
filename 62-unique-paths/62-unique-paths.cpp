54b66572dec2fdd1a8328b35f681fe63573228b4
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-08-01 14:55:51 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         
-        vector<vector<int>>cache(m,vector<int>(n,0))  ; 
+        long long int res = 1; 
+        int i = m+n-2;
+        int j = 1 ; 
         
-        cache[m-1][n-1] = 1;  
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                if(i+1<m) 
-                    cache[i][j]+=cache[i+1][j]  ; 
-                if(j+1<n) 
-                    cache[i][j]+=cache[i][j+1]  ; 
-            }
+        for(;i>=max(m,n);i--,j++){
+            res = (res*i)/j ;
         }
-        
-        return cache[0][0] ; 
+        return res;
     }
 };
