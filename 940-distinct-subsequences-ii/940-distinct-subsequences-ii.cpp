24ef87f46b9d8322ae45cfa5c6c0324ad6083750
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-08-12 16:46:36 +0530

    Time: 416 ms (6.16%), Space: 27.7 MB (6.05%) - LeetHub

---
+++ 940-distinct-subsequences-ii/940-distinct-subsequences-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    typedef long long int ll ; 
+    ll mod = 1e9+7   ; 
+    
+    ll f(ll i, string &s, vector<ll>&dp){
+        ll n = s.size() ;  
+        if(i>=n) return 0 ; 
+        
+        ll res = 0  ;
+        if(dp[i]!=-1) return dp[i]  ; 
+        
+        vector<ll>vis(27,0)  ; 
+       // vis[s[i]-'a'] = 1;  
+        for(ll j = i ; j<n ; ++j){
+            if(vis[s[j]-'a'] == 1) continue;  
+            
+            ll k = 1 + f(j+1,s,dp)%mod  ;
+            k=k%mod ; 
+            res = res % mod + k ;        
+            vis[s[j]-'a'] = 1; 
+        }
+        
+        return dp[i] = res % mod ; 
+    }
+    int distinctSubseqII(string s) {
+        ll n = s.size()  ;
+        vector<ll>dp(n+10,-1)  ;
+        return f(0,s,dp)  ; 
+    }
+};
