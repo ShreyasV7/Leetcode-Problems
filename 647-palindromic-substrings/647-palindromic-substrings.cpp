715e56ca82b6cb264254083a558459f48c569e3f
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-09-09 00:21:41 +0530

    Time: 43 ms (30.65%), Space: 20.4 MB (13.10%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int isPalindrome(int index1, int index2, string &s){
+    int isPalindrome(int index1, int index2, string &s,vector<vector<int>>&cache){
         
-        int left = index1 , right = index2 ; 
+        if(index1 >= index2) return 1;  
         
-        while(left<=right){
-            if(s[left]!=s[right]) return 0 ;  
-            left++ ; 
-            right-- ; 
-        }
-        return 1; 
+        if(cache[index1][index2]!=-1) return cache[index1][index2]  ; 
+        if(s[index1]!=s[index2]) return cache[index1][index2] = 0 ; 
+        
+        return cache[index1][index2] = isPalindrome(index1+1,index2-1,s,cache) ;  
+        
     }
     int countSubstrings(string s) {
         
         int len = s.size()  , total=0  ; 
+        vector<vector<int>>cache(len+1,vector<int>(len+1,-1)) ; 
+        
         for(int index1 = 0 ; index1 < len ; index1++){
             for(int index2 = index1 ; index2 < len ; index2++){
                 
-                total+=isPalindrome(index1,index2,s)  ; 
+                total+=isPalindrome(index1,index2,s,cache)  ; 
             }
         }
         return total ; 
