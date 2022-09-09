c6d7ec7b8adcb25e659653f0be773ada0f7b87a2
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-09-10 01:41:35 +0530

    Time: 30 ms (58.86%), Space: 8.4 MB (77.58%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int cache[101][101][2] ;
+    int f(int i, vector<int>&piles,int m,int alice){
+        
+        int n = piles.size()  ;
+        if(i>=n) return 0 ;
+        int total = 0 , cnt=0 ;  
+        
+        if(cache[i][m][alice]!=-1) return cache[i][m][alice] ;  
+        
+        if(alice==1){
+            int j ; 
+            for(j=1;j<=m*2 ;j++){ 
+                       if(j+i-1<n)
+                        cnt+=piles[j+i-1]  ; 
+                        // m = max(m,j) ; 
+             total = max(total,cnt+f(i+j,piles,max(j,m),0));
+            }
+            //cache[i][m][alice] = total ; 
+        }
+        else{
+           
+            total = INT_MAX;
+            int j  ;
+            for(j=1;j<=m*2 ;j++){
+                //m = max(m,j)  ; 
+                total = min(total,f(i+j,piles,max(j,m),1))  ; 
+            }
+        }
+        
+        return cache[i][m][alice] = total ; 
+    }
+    int stoneGameII(vector<int>& piles) {
+        int n=piles.size() ;  
+        memset(cache,-1,sizeof(cache)) ; 
+        int total = f(0,piles,1,1) ;  
+        return total ; 
+    }
+};
