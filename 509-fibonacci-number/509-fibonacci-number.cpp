feeb260cef697ac30f6f367f760c99bc8970f877
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-09-19 17:56:07 +0530

    Time: 35 ms (5.69%), Space: 5.8 MB (78.53%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,27 +1,8 @@
 class Solution {
 public:
-    class Solutions{
-        
-        public:
-         
-        int fibo(int n){
-            
-            vector<int>dp(n+1,0)  ; 
-            dp[0] = 0 ;  dp[1] = 1;  
-            
-            for(int i=2;i<=n;i++){
-                dp[i] = dp[i-1]+dp[i-2] ; 
-            }
-            
-            return dp[n]  ; 
-        }
-        
-    } ; 
     int fib(int n) {
-        if(n==0) return 0  ; 
-        if(n==1) return 1 ; 
-        Solutions obj ; 
-        int ans = obj.fibo(n)  ;
-        return ans; 
+        if(n==0) return 0 ;  
+        if(n==1) return 1;  
+        return fib(n-2)+fib(n-1)  ; 
     }
 };
