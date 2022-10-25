7a035915b687abff61f5392725b676f6c528bc02
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-10-25 23:44:13 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (56.77%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        
+        string str=to_string(num)  ; 
+        map<int,int>mp ;  
+        
+        int l = str.size() ;  
+        for(int i=0;i<l;++i){
+            mp[str[i]-'0']=i;
+        }
+        
+        for(int i=0;i<l;++i){
+            for(int j=9; j>(str[i]-'0'); j--){
+                
+                if(i < mp[j]){
+                    swap(str[i],str[mp[j]])  ; 
+                    return stoi(str);  
+                }
+            }
+        }
+        return stoi(str) ; 
+    }
+};
