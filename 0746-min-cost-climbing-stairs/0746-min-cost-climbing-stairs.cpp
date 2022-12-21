7ecfcd5d6b56fca6d675dea4f1cf11378092052d
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-12-21 21:51:26 +0530

    Time: 8 ms (74.26%), Space: 13.9 MB (11.39%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -9,7 +9,15 @@ public:
     }
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size() ; 
-        vector<int>dp(n+1,-1) ;  
-        return min(f(0,cost,n,dp),f(1,cost,n,dp)) ;  
+        vector<int>dp(n+1,0) ;  
+       // return min(f(0,cost,n,dp),f(1,cost,n,dp)) ; 
+        
+        dp[0] = cost[0]  ; 
+        dp[1] = cost[1]  ; 
+        
+        for(int i=2;i<n;i++){
+            dp[i] = min(cost[i]+dp[i-1] , cost[i]+dp[i-2])  ; 
+        }
+        return min(dp[n-2],dp[n-1]) ; 
     }
 };
