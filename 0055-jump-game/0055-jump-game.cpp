852e503871c93e2881f5f8f43b0ede560dc6adff
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2022-12-24 01:48:03 +0530

    Time: 71 ms (89.51%), Space: 48.4 MB (48.71%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-    int f(int idx,vector<int>&nums,int n,vector<int>&dp){
-        // if(idx >= n) return 0 ; 
-        if(idx >= n-1) return 1; 
-        if(dp[idx]!=-1) return dp[idx]  ; 
-        for(int j=idx+1; j<=nums[idx]+idx; j++){
-            if(f(j,nums,n,dp)) return dp[idx]=1; 
-        }
-        return dp[idx]=0 ; 
-    }
     bool canJump(vector<int>& nums) {
-        int n=nums.size() ; 
-        vector<int>dp(n+1,-1)  ;
-        int res = f(0,nums,n,dp) ;
-        return res ; 
+        int n=nums.size()  ; 
+        int reach=0;
+        for(int i=0;i<n;++i){
+            if(i>reach) return 0 ; 
+            reach = max(i+nums[i],reach) ; 
+        }
+        return 1; 
     }
 };
