55281b5b1f8bc1d1483b119e62b946e98629adaa
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2023-02-08 21:34:05 +0530

    Time: 164 ms (5.78%), Space: 108.7 MB (5.13%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-         int n = gas.size() ; 
+    int canCompleteCircuit(vector<int>& A, vector<int>& B) {
+         int n = A.size() ; 
         int total_surplus = 0; // it will give us a difference b/w gas & cost
         int surplus = 0; // our tank
         int start = 0; // and the index of gas station
         
         for(int i = 0; i < n; i++){
-            total_surplus += gas[i] - cost[i];
-            surplus += gas[i] - cost[i];
+            total_surplus += A[i] - B[i];
+            surplus += A[i] - B[i];
             if(surplus < 0){ // if the tank goes -ve
                 surplus = 0; // reset our tank
                 start = i + 1; // and update the stating gas station
