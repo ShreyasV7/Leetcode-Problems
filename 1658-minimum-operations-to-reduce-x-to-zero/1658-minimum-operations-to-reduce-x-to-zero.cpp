8158a55a8434fdfe290743a85aa3af640ee2d0ea
Shreyas Vishwakarma <73514168+ShreyasV7@users.noreply.github.com>
2023-09-21 00:59:29 +0530

    Time: 122 ms (68.39%), Space: 98.8 MB (38.49%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         
-        int n = nums.size()  ; 
-        int total=0 ;
-        for(int i=0;i<n;i++){
-            total+=nums[i]  ; 
+        int total = 0 ; 
+        int n = nums.size();  
+        
+        for(int i=0 ; i<n; i++){
+           total += nums[i]  ; 
         }
         
         total-=x ; 
-    
+        
         if(total==0) return n ; 
         
-        int sum = 0 , len=0 ; 
+        int res=0;  
         
-        int left=0,right=0; 
+        int left = 0 , sum=0; 
         
-        for(;right<n;right++){
-            
+        for(int right = 0 ; right < n ; right++){
             sum+=nums[right]  ; 
             
             while(left < n && sum > total){
@@ -25,12 +25,15 @@ public:
                 left++ ; 
             }
             
-            if(total==sum){
-                len = max(right-left+1,len)  ; 
-            }
+            if(sum==total) res = max(res,right-left+1); 
         }
         
-        if(len==0)return -1 ;
-        return n-len; 
+        if(!res) return -1 ; 
+        return n-res ; 
+        
+        /*
+         time complexity = O(n)
+         space complexity = O(1)
+        */
     }
 };
